e0d586da24a503fb83aecaefacc92b36f6adf841
psavov256@gmail.com <psavov256@gmail.com>
2021-12-04 12:13:01 +0000

    Day 4 Python

---
+++ day4.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+with open("input4.txt") as f:
+    nums, *boards = f.read().strip().split("\n\n")
+
+nums = [int(n) for n in nums.split(",")]
+
+boards = [
+    [[int(n) for n in line.split()] for line in board.split("\n")] for board in boards
+]
+
+boards = [np.array(board) for board in boards]
+
+
+def check(board):
+    if any(board.sum(0) == -5) or any(board.sum(1) == -5):
+        return True
+    return False
+
+
+def update(board, num):
+    new = board.copy()
+    new[new == num] = -1
+
+    return new
+
+
+n_boards = len(boards)
+wins = [0] * len(boards)
+
+for n in nums:
+    for i in range(n_boards):
+        boards[i] = update(boards[i], n)
+        if wins[i] == 0 and check(boards[i]):
+            res = n * (boards[i][boards[i] != -1].sum())
+            wins[i] = 1
+            if sum(wins) == 1:
+                first = res
+            if sum(wins) == n_boards:
+                last = res
+                break
+    if sum(wins) == n_boards:
+        break
+
+print(first, last)
