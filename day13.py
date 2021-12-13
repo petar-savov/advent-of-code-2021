eceff2a669efdec98e16542eecaeb4cd97b8b170
psavov256@gmail.com <psavov256@gmail.com>
2021-12-13 11:52:49 +0000

    Day 13 Python Part 2 with numpy and matplotlib

--- day13.py
+++ day13.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 with open("input13.txt") as f:
     nums, folds = f.read().split("\n\n")
 
@@ -7,20 +10,29 @@ folds = [
     for line in folds.strip().split("\n")
 ]
 
-fdir, faxis = folds[0]
-new = []
-for x, y in nums:
-    if fdir == "x":
-        if x < faxis:
-            new.append((x, y))
-        elif x > faxis:
-            newx = faxis - (x - faxis)
-            new.append((newx, y))
-    if fdir == "y":
-        if y < faxis:
-            new.append((x, y))
-        elif y > faxis:
-            newy = faxis - (y - faxis)
-            new.append((x, newy))
+# could also be done with np.flip
+for fdir, faxis in folds:
+    new = []
+    for x, y in nums:
+        if fdir == "x":
+            if x < faxis:
+                new.append((x, y))
+            elif x > faxis:
+                newx = faxis - (x - faxis)
+                new.append((newx, y))
+        if fdir == "y":
+            if y < faxis:
+                new.append((x, y))
+            elif y > faxis:
+                newy = faxis - (y - faxis)
+                new.append((x, newy))
+    nums = new
 
-print(len(set(new)))
+
+xmax, ymax = max(n[0] for n in nums), max(n[1] for n in nums)
+arr = np.zeros((ymax + 1, xmax + 1))
+for x, y in nums:
+    arr[y, x] = 1
+
+plt.imshow(arr)
+plt.show()
